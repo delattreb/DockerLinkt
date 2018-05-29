914b3f41a6f7f0e6e33d205e6c3a018e777eaa02
delattreb <delattreb@gmail.com>
2018-05-29 22:30:53 +0200

    Patch doc install docker-compose

--- doc/install.sh
+++ doc/install.sh
@@ -1,4 +1,5 @@
 # Install Docker-compose
+apt-get install -y docker-compose
 curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
